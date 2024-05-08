e8b5237d40ab2a1554a7e6fdbf80581ef9431e98
htetsoemoe <soemoehtet.mdy@gmail.com>
2024-05-08 11:42:58 +0630

    configure custom proxy rules for the dev server

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    // Configure custom proxy rules for the dev server. Expects an object of { key: options } pairs. Uses http-proxy.
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3500',
+        secure: false,
+      }
+    }
+  },
   plugins: [react()],
 })
